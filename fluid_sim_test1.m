6039a8999fd0008b9fdd0561d0a2f26baaa1f86c
spiggen <jonasvilgotlotberg@hotmail.se>
2024-01-18 14:02:47 +0100

    update

--- fluid_sim_test1.m
+++ fluid_sim_test1.m
@@ -7,7 +7,7 @@ warning("off")
 myax = axes();
 fireflies();
 
-dt = 0.0001;
+dt = 0.00001;
 
 x_vec = linspace(0,10,200);
 y_vec = linspace(0,10,200);
@@ -19,15 +19,16 @@ mass(:,:) = pi*10;
 
 
 while true
-%v_x(80:120,100:101) = -1;
-mass(60:140, 100:120) = 10*pi + sin(linspace(0,pi,81)')*sin(linspace(0,2*pi, 21));
+v_x(80:120,100:101) = - 10;
+%mass(60:140, 100:120) = 10*pi + sin(linspace(0,pi,81)')*sin(linspace(0,2*pi, 21));
 step_velocity
 
 
 
-imagesc(myax, x_mesh(1,1:end), y_mesh(1:end,1), mass);
+imagesc(myax, x_mesh(1, 1:end), y_mesh(1:end,1), log(mass));
 myax.NextPlot = "add";
-quiver(myax,x_mesh(1:4:end,1:4:end), y_mesh(1:4:end, 1:4:end), v_x(1:4:end, 1:4:end),v_y(1:4:end, 1:4:end))
+%quiver(myax,x_mesh(1:4:end,1:4:end), y_mesh(1:4:end, 1:4:end), v_x(1:4:end, 1:4:end),v_y(1:4:end, 1:4:end))
+streamslice(myax,x_mesh(1:1:end,1:1:end), y_mesh(1:1:end, 1:1:end), v_x(1:1:end, 1:1:end),v_y(1:1:end, 1:1:end))
 myax.NextPlot = "replacechildren";
 xlim(myax, [0,10]);
 ylim(myax, [0,10]);
--- step_velocity.m
+++ step_velocity.m
@@ -25,16 +25,16 @@ make_jacobian
 %v_y = strange2zero(v_y);
 
 
-mass = mass + dm_dx.*(x_prev - x_mesh) - mass.*dvx_dx*dt*dy + ...
-              dm_dy.*(y_prev - y_mesh) - mass.*dvy_dy*dt*dx;  
+mass = mass + dm_dx.*(x_prev - x_mesh) - 2*mass.*dvx_dx*dt + ...
+              dm_dy.*(y_prev - y_mesh) - 2*mass.*dvy_dy*dt;  
 
 %mass = strange2zero(mass);
 mass(mass  < 9*pi) = 9*pi;
 mass(12*pi < mass) = 12*pi;
 
 
-v_x = v_x - 10000*dt.*dm_dx*dx./(dy.*mass);
-v_y = v_y - 10000*dt.*dm_dy*dx./(dx.*mass);
+v_x = v_x - 100000*dt.*dm_dx*dx./(dy.*mass);
+v_y = v_y - 100000*dt.*dm_dy*dx./(dx.*mass);
 
 
 v_x = strange2zero(v_x);
@@ -48,16 +48,29 @@ function [prev_x,prev_y] = solve_for_previous_position(J_11, J_12, ...
                                                        x_0,  y_0, ...
                                                        v_x0, v_y0)
 
-dt = evalin("base", "dt"); % <-- Not pretty but makes me not have to rewrite 10^30 inputs whenever I arrayfun dis bitch
+% dt = evalin("base", "dt"); % <-- Not pretty but makes me not have to rewrite 10^30 inputs whenever I arrayfun dis bitch
+% 
+% Jacobian = [J_11,J_12;
+%             J_21,J_22];
+% 
+% pos_vec  = [x_0; y_0 ];
+% v_vec    = [v_x0;v_y0];
+% 
+% 
+% prev_pos = pos_vec - (eye(2) + Jacobian*dt)\v_vec*dt;
+% prev_x = prev_pos(1);
+% prev_y = prev_pos(2);
 
-Jacobian = [J_11,J_12;
-            J_21,J_22];
 
-pos_vec  = [x_0; y_0 ];
-v_vec    = [v_x0;v_y0];
+dt = 0.00001;
 
+J_11 = J_11*dt + 1;
+J_12 = J_12*dt + 0;
+J_21 = J_21*dt + 0;
+J_22 = J_22*dt + 1;
 
-prev_pos = (eye(2) + Jacobian*dt)\( (eye(2) + Jacobian*dt)*pos_vec - v_vec*dt);
-prev_x = prev_pos(1);
-prev_y = prev_pos(2);
+D = J_11*J_22 - J_21*J_12;
+
+prev_x = x_0 - (  J_22*v_x0 - J_12*v_y0)*dt/D;
+prev_y = y_0 - (- J_21*v_x0 + J_11*v_y0)*dt/D;
 end
